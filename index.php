16702e201ea0bd4c4461c396e9b202545df7a22d
Shitov56 <99617571+Shitov56@users.noreply.github.com>
2022-02-16 17:19:42 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+        include 'php/db.php';
+    ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ГрумRoom</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+      <header class="header">
+            <div class="header-logo">
+     
+                <img src="./ГрумRoom_files/1.png" alt="" srcset="" class="header-img">
+            </div>
+
+      </div>
+          </div>
+    <div class="contener">
+      <?php
+      include "php/header.php"
+      ?>
+    </div>
+</body>
+</html>
